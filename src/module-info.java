902d030f456d69edb008cb00aa97f3ecdf31662b
PSamudralaSamudrala <samudraapps914@gmail.com>
2020-11-27 14:32:25 +0530

    welcometodevops

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Welcometohenkel {
+}
---
+++ src/welcometodevops/Welcometodevops.java
@@ -0,0 +1,10 @@
+package welcometodevops;
+public class Welcometodevops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WelcometoDevops");
+
+	}
+
+}
